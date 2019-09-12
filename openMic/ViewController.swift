2f32a4f5e683c24c812bc92c863a880165378f09
Adrian Wyllie <adrian@dilex.net>
2019-09-12 13:38:52 -0400

    Initial Commit

---
+++ openMic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  openMic
+//
+//  Created by Adrian Wyllie on 9/12/19.
+//  Copyright © 2019 Adrian Wyllie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
